b5d3ea11744ce8e5b90f24f834f3a75210de473f
Mayur Sehgal <its.mayur718@gmail.com>
2023-10-17 02:26:07 +0530

    lets start

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <div className="bg-black"></div>
+      <div className="content">
+        <div className="">
+          <Image src="CScircle.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
